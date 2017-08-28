21380f9b5a4e8cd2f412e20b2c20f5c1dc996f1c
Lucas Queiroz <lucas.queiroz@moip.com.br>
2017-08-28 15:52:09 -0300

    Add bank data

--- refactor/bank/bank.rb
+++ refactor/bank/bank.rb
@@ -1,3 +1,9 @@
-class Bank
-  def initialize(code, account_min_digits, account_max_digits)
+require_relative '../data_class'
+
+# Data for the banks
+class Bank < DataClass
+  def initialize(code, agency_rule, account, account_check)
+    super({ code: code, agency_rule: agency_rule, account: account,
+            account_check: account_check })
+  end
 end
